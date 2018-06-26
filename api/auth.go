1b9f7ee39a69bc24796c82da3afc070bf41a937a
Marcus Weiner <marcus.weiner@gmail.com>
2018-06-27 01:12:12 +0200

    :art: Refactor api key check into function

--- api/auth.go
+++ api/auth.go
@@ -1,16 +1,26 @@
 package api
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
 )
 
+func IsValidApiKey(ctx *gin.Context, validKey string) error {
+	apiKey := ctx.GetHeader("Authorization")
+	if apiKey != validKey {
+		return errors.New("Invalid API key")
+	}
+
+	ctx.Set("apiKey", validKey)
+
+	return nil
+}
+
 func HandleApiKeyAuth(validKey string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		apiKey := c.GetHeader("Authorization")
-		if apiKey != validKey {
-			c.AbortWithStatus(403)
+		if err := IsValidApiKey(c, validKey); err != nil {
+			c.AbortWithStatus(401)
 		}
-
-		c.Set("apiKey", apiKey)
 	}
 }
